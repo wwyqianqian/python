7eb1f34d8899d7fc6612fc4bf2025a388f0f9066
wwyqianqian <1208331933@qq.com>
2017-11-11 21:00:50 +0800

    drawSnake by turtle

---
+++ py_for_beginners/DrawSnake.py
@@ -0,0 +1,18 @@
+import turtle
+def drawSnake(rad, angle, len, neckrad):
+    for i in range(len):
+        turtle.circle(rad, angle)
+        turtle.circle(-rad, angle)
+    turtle.circle(rad, angle/2)
+    turtle.fd(rad)
+    turtle.circle(neckrad+1, 180)
+    turtle.fd(rad*2/3)
+
+def main():
+    turtle.setup(1500, 800, 0, 0)
+    pythonsize = 30
+    turtle.pencolor("red")
+    turtle.seth(-40)
+    drawSnake(40, 80, 5, pythonsize/2)
+
+main()
