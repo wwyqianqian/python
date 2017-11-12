ad57fc1a72f277fa80872e2263b893b72765115a
wwyqianqian <1208331933@qq.com>
2017-11-12 21:34:37 +0800

    pi

--- py_for_beginners/TempConvert.py
+++ py_for_beginners/TempConvert.py
@@ -1,9 +1,9 @@
 val = input("Please input the temperature (Celsius or Fahrenheit) :")
 if val[-1] in ['C', 'c']:
     f = 1.8 * float(val[0:-1]) + 32
-    print("It is %.2fF"%f)
+    print("It is %.2fF" % f)
 elif val[-1] in ['F', 'f']:
     c = (float(val[0:-1]) - 32) / 1.8
-    print("It is %.2fC"%c)
+    print("It is %.2fC" % c)
 else:
     print("type error")
