e7b4a2baa5eac690868f0d459dbc4c1ea8d4a04c
wwyqianqian <1208331933@qq.com>
2018-06-05 20:54:00 +0800

    [fix]: fix 3 typo REQ/stock.py

--- py_for_beginners/REQ/stock.py
+++ py_for_beginners/REQ/stock.py
@@ -1,53 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import traceback
 
 
 def getHTMLText(url):
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
-        r.encoding = r.apparent_encoding
-        return t.text
-    except:
-        return ""
+    r = requests.get(url)
+    r.encoding = r.apparent_encoding
+    return r.text
 
 def getStockList(lst, stockURL):
     html = getHTMLText(stockURL)
     soup = BeautifulSoup(html, 'html.parser')
     a = soup.find_all('a')
     for i in a:
-        try:
-            herf = i.attrs['href']
-            lst.append(re.findall(r"[s][hz]\d{6}", href)[0])
-        except:
-            continue
+        href = i.attrs['href']
+        lst.append(re.findall(r"[s][hz]\d{6}", href)[0])
 
-def getStockInfo(lst, stockURL, fapth):
+def getStockInfo(lst, stockURL, fpath):
     for stock in lst:
         url = stockURL + stock + ".html"
         html = getHTMLText(url)
-        try:
-            if html == "":
-                continue
-            infoDict = {}
-            soup = BeautifulSoup(html, 'html.parser')
-            stockInfo = soup.find('div', attrs = {'class':'stock-bets'})
-            name = stockInfo.find_all(attrs = {'class':'bets-name'})[0]
-            infoDict.update({'股票名称': name.text.split()[0]})
-            keyList = stockInfo.find_all('dt')
-            valueList = stockInfo.find_all('dd')
-            for i in range(len(keyList)):
-                key = keyList[i].text
-                val = valueList[i].text
-                infoDict[key] = val
-            with open(fpath, 'w', encoding = 'utf-8') as f:
-                f.write(str(infoDict) + '\n')
-                print("ok")
-        except:
-            traceback.print_exc()
+        if html == "":
             continue
+        infoDict = {}
+        soup = BeautifulSoup(html, 'html.parser')
+        stockInfo = soup.find('div', attrs = {'class':'stock-bets'})
+        name = stockInfo.find_all(attrs = {'class':'bets-name'})[0]
+        infoDict.update({'股票名称': name.text.split()[0]})
+        keyList = stockInfo.find_all('dt')
+        valueList = stockInfo.find_all('dd')
+        for i in range(len(keyList)):
+            key = keyList[i].text
+            val = valueList[i].text
+            infoDict[key] = val
+        with open(fpath, 'w', encoding = 'utf-8') as f:
+            f.write(str(infoDict) + '\n')
 
 def main():
     stock_list_url = 'http://quote.eastmoney.com/stocklist.html'
