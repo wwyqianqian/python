86c6333e1dc1754e690c9b462ad37b9cd5171a4c
wwyqianqian <1208331933@qq.com>
2018-06-10 10:23:28 +0800

    [feat]: add Spider/BaiduStocks stocks.py pipelines.py settings.py

--- Spider/BaiduStocks/BaiduStocks/pipelines.py
+++ Spider/BaiduStocks/BaiduStocks/pipelines.py
@@ -9,3 +9,18 @@
 class BaidustocksPipeline(object):
     def process_item(self, item, spider):
         return item
+
+class BaidustocksInfoPipeline(object):
+    def open_spider(self, spider):
+        self.f = open('BaiduStockInfo.txt', 'w')
+
+    def close_spider(self, spider):
+        self.f.close()
+
+    def process_item(self, item, spider):
+        try:
+            line = str(dict(item)) + '\n'
+            self.f.write(line)
+        except:
+            pass
+        return item
